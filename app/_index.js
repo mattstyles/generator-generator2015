97080402fbf292d250787d68743f70bc19e2ede5
mattstyles <matt@veryfizzyjelly.com>
2015-05-12 12:15:37 +0100

    #add copy template directory

--- app/_index.js
+++ app/_index.js
@@ -1,12 +1,25 @@
 
+import path from 'path'
+
 import { Base } from 'yeoman-generator'
 import chalk from 'chalk'
 import yosay from 'yosay'
 import osenv from 'osenv'
+import glob from 'glob'
 
 export default class UrbanGenerator extends Base {
     constructor( ...args ) {
         super( ...args )
+
+        this.option( 'skip-prompt', {
+            desc: 'Skips the prompt and uses defaults',
+            required: false,
+            defaults: false
+        })
+
+        if ( process.env.DEBUG ) {
+            this.destinationRoot( path.join( __dirname, '/../debug' ) )
+        }
     }
 
     pkg = require( '../package.json' )
@@ -23,26 +36,68 @@ export default class UrbanGenerator extends Base {
     }, {
         name: 'authorName',
         message: 'What is the author name?',
-        default: osenv.user()
+        default: osenv.user(),
+        store: true
     }, {
         name: 'userName',
         message: 'What is your github username?',
-        default: osenv.user().toLowerCase().replace( /\s/g, '' )
+        default: osenv.user().toLowerCase().replace( /\s/g, '' ),
+        store: true
     }]
 
     hello() {
         this.log( yosay([
-            chalk.cyan( 'Urban Web Client' ),
+            chalk.cyan( 'Generator es2015' ),
             'Feed me information...'
         ].join( '\n' )))
     }
 
-    app() {
+    prompting() {
+        if ( !this.options[ 'skip-install' ] ) {
+            this.log( 'Skipping prompt' )
+            this.props = {
+                projectName: 'debug-project',
+                projectDescription: 'project description',
+                authorName: 'Arthur Debug',
+                userName: 'adebug'
+            }
+            return
+        }
+
         this.prompt( UrbanGenerator.prompts, props => {
             this.props = props
+        })
+    }
 
-            console.log( 'all done' )
-            console.log( props )
+    app() {
+        var done = this.async()
+
+        this.log( 'Copying templates' )
+
+        glob( path.join( this.sourceRoot(), '*' ), {
+            dot: true
+        }, ( err, files ) => {
+            if ( err ) {
+                throw new Error( err )
+            }
+
+            files.map( file => {
+                return file.replace( this.sourceRoot(), '' )
+            })
+                .forEach( file => {
+                    this.fs.copyTpl(
+                        this.templatePath( file ),
+                        this.destinationPath( file ),
+                        this.props,
+                        {
+                            evaluate: /\{\{\{(.+?)\}\}\}/g,
+                            interpolate: /\{\{(.+?)\}\}/g,
+                            escape: /\{\{-(.+?)\}\}/g
+                        }
+                    )
+                })
+
+            done()
         })
     }
 }
--- test.js
+++ test.js
@@ -4,6 +4,9 @@ import minimist from 'minimist'
 
 let argv = minimist( process.argv.slice( 2 ) )
 
+/**
+ * Require each file to run tests held within
+ */
 argv._.forEach( file => {
     require( path.resolve( file ) )
 })
